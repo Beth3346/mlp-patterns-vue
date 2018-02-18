71f1eab897878cc604527cbac13d13fe1c91c6a4
Elizabeth Rogers <beth3346@gmail.com>
2018-02-18 10:41:09 -0600

    add headings to list examples

--- src/components/patterns/00-elements/ListItems.vue
+++ src/components/patterns/00-elements/ListItems.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+    <h3 class="pattern-heading">Unordered List</h3>
     <ul>
       <li v-for="pony in ponies">{{pony.name}}</li>
     </ul>
+    <h3 class="pattern-heading">Ordered List</h3>
     <ol>
       <li v-for="pony in ponies">{{pony.name}}</li>
     </ol>
+    <h3 class="pattern-heading">Inline Unordered List</h3>
     <ul class="elr-inline-list">
       <li v-for="pony in ponies">{{pony.name}}</li>
     </ul>
@@ -23,3 +26,9 @@
     }
   }
 </script>
+
+<style>
+  .pattern-heading {
+    padding-top: 20px;
+  }
+</style>
